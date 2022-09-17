3be7d48037f9c6953fd34956025861ccc4f9fa9f
Deng Ming <mingflycash@gmail.com>
2022-09-17 15:59:59 +0800

    slice: FilterMap 和 Delete 方法

--- internal/errs/error.go
+++ internal/errs/error.go
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package list
+package errs
 
 import "fmt"
 
-// newErrIndexOutOfRange 创建一个代表
-func newErrIndexOutOfRange(length int, index int) error {
+// NewErrIndexOutOfRange 创建一个代表下标超出范围的错误
+func NewErrIndexOutOfRange(length int, index int) error {
 	return fmt.Errorf("ekit: 下标超出范围，长度 %d, 下标 %d", length, index)
 }
